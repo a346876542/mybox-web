d7b83e479a19c56b5472771769e4dec794c6dec0
王志昂 <670578767@qq.com>
2025-04-17 16:52:34 +0800

    第一次提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,13 @@
   </div>
   <div style="text-align: center;font-size: 30px">
     {{ a }}
+    {{ b }}
     {{ data.a }}
     {{ data.b }}
   </div>
   <div>
     <input v-model="data.a"/>
+    <input v-model="b"/>
   </div>
   <div>
     <span style="color: red" v-if="data.name==='双鞭呼延灼'">双鞭呼延灼</span>
@@ -21,6 +23,20 @@
     </select>
   </div>
 
+  <div>
+    <button v-on:click="add">点我加好运</button>
+    <button @click="add">点我加好运</button>
+    <button @click="add1(100)">点我加好运</button>
+  </div>
+
+  <div>
+    <div :style="{'with':'100px','height':'100px','backgroundColor':'yellow'}"></div>
+    <div :style="data.box"></div>
+    <img :src="'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'" alt="icon">
+  </div>
+  <div>
+    <img :src="data.img" alt="icon">
+  </div>
 
 </template>
 <!--不写 setup 会报错 -->
@@ -28,13 +44,31 @@
 //先要导包a
 import {reactive, ref} from "vue"
 
-const a = ref(1)
+let a = ref(1)
+
+let b = ref(100)
+
 
 const data = reactive({
   a: 1,
   b: 3,
   name: '小李广花荣',
-  arr: ['香蕉', '苹果', '橘子']
+  arr: ['香蕉', '苹果', '橘子'],
+  box: {
+    width: "100px",
+    height: "100px",
+    backgroundColor: 'red',
+  },
+  img: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+
 })
+
+const add = () => {
+  a.value++
+}
+
+const add1 = (item) => {
+  b.value += item
+}
 
 </script>
